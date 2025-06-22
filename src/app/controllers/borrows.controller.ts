38ededee8330211c032ab2eee8a74c1f1565fb4a
ratul0407 <rajaulislamratul12@gmail.com>
2025-06-22 22:56:39 +0600

    added the aggregate route to borrow controller

--- src/app/controllers/borrows.controller.ts
+++ src/app/controllers/borrows.controller.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { Borrow } from "../models/borrows.models";
 import { Book } from "../models/books.model";
-import { IBook } from "../interfaces/books.interface";
 
 export const borrowRoutes = express.Router();
 
@@ -33,6 +32,50 @@ borrowRoutes.post("/", async (req: Request, res: Response) => {
       success: false,
       message: "Failed to borrow the book",
       error: error,
+    });
+  }
+});
+
+borrowRoutes.get("/", async (req: Request, res: Response) => {
+  try {
+    const result = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      { $unwind: "$bookDetails" },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookDetails.title",
+            isbn: "$bookDetails.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: true,
+      message: "Failed to load the borrow data",
+      error,
     });
   }
 });
